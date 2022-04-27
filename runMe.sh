58a24e48dc77708b54c83296fda8b44c4735fc70
Abdelrahman Abdelwahab <abdoahmarwan@gmail.com>
2022-04-27 12:25:53 +0200

    Update runMe.sh

--- runMe.sh
+++ runMe.sh
@@ -1,84 +1,84 @@
+# HomeBrew-Script
+
 #!/bin/sh
 
 # Homebrew Script for OSX
-# To execute: save and `chmod +x ./runMe.sh` then `./runMe.sh`
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
 echo "Installing brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing my best Homebrew Packages... "
-echo "==> Formulae"
-    brew install cocoapods
-    brew install tldr
-    brew install archey
-    brew install chruby
+# Homebrew Packages
+echo "Installing homebrew Packages... "
+    brew install neofetch            # custom terminal - 1
+    brew install zsh                 # custom terminal - 2
+    brew install zsh-completions     # custom terminal - 3
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    brew install ruby-install        # development package
+    brew install cocoapods           # development package
+    brew install youtube-dl          # download videos --> youtube-dl "https://www.youtube.com/video"
     brew install curl
-    brew install fastlane
-    brew install legit
-    brew install mackup
-    brew install neofetch
-    brew install neovim
-    brew install node
-    brew install ruby-install
-    brew install smartmontools
-    brew install starship
-    brew install subversion
-    brew install tldr
-    brew install trash
-    brew install tree
     brew install wget
-    brew install youtube-dl
-    brew install zsh
-    brew install zsh-completions
-    brew install fswatch
-    brew install coreutils
-    brew install aria2 # https://github.com/aria2/aria2
-    brew install you-get
+    brew install tailor
+
+# Dev Tools
+echo "Installing development tools..."
     brew install git
     brew install git-extras
     brew install git-flow
     brew install git-secrets
-    #brew install ffmpeg
-echo "==> Casks"
-    #brew install --cask Jellyfin
+
+# Dev Apps
+echo "Installing development apps..."
     brew install --cask Docker
-    brew install --cask alacritty
-    brew install --cask amethyst
-    brew install --cask forticlient
-    brew install --cask vlc
     brew install --cask xcodes
     brew install --cask android-studio
-    brew install --cask virtualbox
-    brew install --cask gitkraken
+    brew install --cask visual-studio-code
+    brew install --cask sourcetree
     brew install --cask github
+    brew install --cask postman
+    brew install --cask proxyman
+    brew install --cask forticlient
+    brew install --cask amethyst # Tiling window manager for macOS along the lines of xmonad.
 
+# Communication Apps
+echo "Installing communication apps..."
+    brew install --cask slack
+    brew install --cask zoom
+    brew install --cask telegram
+    brew install --cask whatsapp
+    brew install --cask skype
+
+# UI/UX Apps
+echo "Installing UI/UX apps..."
+    brew install --cask adobe-creative-cloud
+
+# General Apps
+echo "Installing general apps..."
+    brew install --cask google-chrome
+
+# Media player Apps
+echo "Installing media player apps..."
+    brew install --cask vlc
+
+# File Storage
+echo "Installing file storage tools..."
+    # brew install --cask onedrive
+    # brew install --cask megasync
+
+# Microsoft Apps
 echo "Installing microsoft apps..."
-brew install --cask visual-studio-code
-brew install --cask microsoft-teams
-brew install --cask microsoft-outlook
-brew install --cask microsoft-word
-brew install --cask onedrive
-
-brew install --cask slack
-brew install --cask zoom
-brew install --cask telegram
-
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask postman
-
+    brew install --cask microsoft-teams
+    # brew install --cask microsoft-word
+    # brew install --cask microsoft-excel
+    # brew install --cask microsoft-powerpoint
 
 # Other
 #echo "Installing everything else..."
 
 
 # List of all packages installed Homebrew
-# brew list
-# brew leaves > my_brews.txt
-# install them on the new mac
-# xargs brew install < my_brews.txt
-#brew uninstall appName to unInstall app
+# --> brew list
 
 #https://betterprogramming.pub/my-top-homebrew-packages-for-2020-b6565d46de85
-
 #sh ./File.sh
